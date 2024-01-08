e818603380116772e09273216d2969ab1013df6d
NghiaTrongdev <133744136+NghiaTrongdev@users.noreply.github.com>
2024-01-08 21:58:54 +0700

    Change constants by ConstantsTemplate instead

---
+++ Youtube/ConstansTemplate.swift
@@ -0,0 +1,17 @@
+//
+//  ConstansTemplate.swift
+//  Youtube
+//
+//  Created by Trọng Nghĩa Nguyễn on 08/01/2024.
+//
+
+import Foundation
+
+
+//
+//struct Constants {
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = " https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(PLAYLIST_ID)&key=\(API_KEY)"
+//}
+//
